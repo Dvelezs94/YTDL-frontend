2ee0317a7deb2629b8eb9ee29b505c2cde8595c0
oh-diego-velez <diego.sanchez@outcomehealth.com>
2021-10-04 18:57:27 -0500

    change icons

--- src/components/ToolDescription.vue
+++ src/components/ToolDescription.vue
@@ -36,7 +36,7 @@
             <div class="flex items-center space-x-3">
                <div class="flex items-center justify-center h-12 w-12 rounded-lg bg-blue-500 text-white">
                   <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 15v2m-6 4h12a2 2 0 002-2v-6a2 2 0 00-2-2H6a2 2 0 00-2 2v6a2 2 0 002 2zm10-10V7a4 4 0 00-8 0v4h8z"></path>
+                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 10V3L4 14h7v7l9-11h-7z"></path>
                   </svg>
                </div>
                <h3 class="text-lg font-semibold">LISTEN ANYWHERE</h3>
@@ -47,7 +47,8 @@
             <div class="flex items-center space-x-3">
                <div class="flex items-center justify-center h-12 w-12 rounded-lg bg-blue-500 text-white">
                   <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 10V3L4 14h7v7l9-11h-7z"></path>
+                     
+                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 15v2m-6 4h12a2 2 0 002-2v-6a2 2 0 00-2-2H6a2 2 0 00-2 2v6a2 2 0 002 2zm10-10V7a4 4 0 00-8 0v4h8z"></path>
                   </svg>
                </div>
                <h3 class="text-lg font-semibold">NO POPUPS</h3>
